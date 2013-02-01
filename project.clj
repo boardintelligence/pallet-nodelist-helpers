0035ce4fe65e674cce32429077fc7f0a5f13c947
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-02-01 17:54:30 +0000

    Upgrade to new pallet api as of 0.8.0-alpha.8.
    Loving the new one!

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://boardintelligence.mit-license.org"}
 
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.cloudhoist/pallet "0.8.0-alpha.7" :exclusions [useful]]
+                 [org.cloudhoist/pallet "0.8.0-alpha.8"]
                  [ch.qos.logback/logback-classic "1.0.7"]]
 
-  :dev-dependencies [[org.cloudhoist/pallet "0.8.0-alpha.7" :type "test-jar"]
+  :dev-dependencies [[org.cloudhoist/pallet "0.8.0-alpha.8" :type "test-jar"]
                      [org.cloudhoist/pallet-lein "0.5.2"]]
 
   :profiles {:dev
-             {:dependencies [[org.cloudhoist/pallet "0.8.0-alpha.7" :classifier "tests"]]
+             {:dependencies [[org.cloudhoist/pallet "0.8.0-alpha.8" :classifier "tests"]]
               :plugins [[org.cloudhoist/pallet-lein "0.5.2"]]}}
 
   :local-repo-classpath true
